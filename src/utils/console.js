af3abce36ee5306b7ca2bfe70548145bc9fb5599
yuzheng14 <422450117@qq.com>
2022-11-21 21:41:51 +0800

    feat(.eslintrc.json console.js): add rules (no-console)
    It can't use console except in src/utils/console.js

---
+++ src/utils/console.js
@@ -0,0 +1,23 @@
+import reportWebVitals from '../../webVitals'
+
+const runInDev = (func) => {
+  if (import.meta.env.MODE === 'development') {
+    func instanceof Function && func()
+  }
+}
+
+export const log = (...args) => {
+  runInDev(() => console.lop(...args))
+}
+
+export const warn = (...args) => {
+  runInDev(() => console.warn(...args))
+}
+
+export const error = (...args) => {
+  runInDev(() => console.error(...args))
+}
+
+export const logWebVitals = () => {
+  runInDev(() => reportWebVitals(console.log))
+}
